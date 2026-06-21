8bb33340bb76c1acc71b4750bbc9d3f416092dad
Nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-06-21 15:06:35 +0200

    removed commented out receive async code

--- src/shopfloor/Mes.Shopfloor.Core/Messaging/Consumer/ListeningRoutine/ConsumerBackgroundService.cs
+++ src/shopfloor/Mes.Shopfloor.Core/Messaging/Consumer/ListeningRoutine/ConsumerBackgroundService.cs
@@ -47,7 +47,7 @@ internal sealed class ConsumerBackgroundService(
             noWait: configuration.QueueOptions.NoWait,
             cancellationToken: cancellationToken);
 
-        // Limit how many messages are accepted if the queue piles up
+        // Limit how many messages are accepted if the queue piles up (prefect count)
         await channel.BasicQosAsync(
             prefetchSize: 0,
             prefetchCount: configuration.PrefetchCount,
@@ -70,75 +70,6 @@ internal sealed class ConsumerBackgroundService(
         var consumer = new AsyncEventingBasicConsumer(channel);
         consumer.ReceivedAsync += (_, args) => ReceiveAsync(args, ackLock, channel, configuration, cancellationToken);
         
-        // consumer.ReceivedAsync += async (_, args) =>
-        // {
-        //     ConsumptionResultCode code;
-        //
-        //     try
-        //     {
-        //         // Fetch message type
-        //         if (!MessageTypeResolver.TryResolveType(args.BasicProperties.Type, out var messageType))
-        //             throw new InvalidOperationException("Failed to resolve message type.");
-        //
-        //         // Fetch the consumption handle method
-        //         if (!ConsumptionMethodResolver.TryReflectMethod(messageType, out var handleAsync))
-        //             throw new InvalidOperationException($"Failed to reflect method '{nameof(IConsumption<>.HandleAsync)}' from '{typeof(IConsumption<>).MakeGenericType(messageType)}'.");
-        //
-        //         // Read the message body as a string
-        //         var json = Encoding.UTF8.GetString(args.Body.Span);
-        //         if (messageType == null || string.IsNullOrWhiteSpace(json))
-        //             throw new InvalidOperationException("Failed to deserialize message.");
-        //
-        //         // Execute deserialization and consume in a fresh service scope
-        //         var message = MessageSerializer.Deserialize(messageType, json);
-        //         using var scope = _serviceProvider.CreateScope();
-        //         {
-        //             ConsumptionResult? combinedResult = null;
-        //
-        //             var consumptionType = typeof(IConsumption<>).MakeGenericType(messageType);
-        //             var consumptions = scope.ServiceProvider.GetServices(consumptionType);
-        //             foreach (var consumption in consumptions)
-        //             {
-        //                 if (handleAsync.Invoke(consumption, parameters: [message, cancellationToken]) is not Task<ConsumptionResult> task)
-        //                     throw new InvalidOperationException($"Failed to handle '{messageType}'.");
-        //             
-        //                 var currentResult = await task;
-        //                 combinedResult = combinedResult == null ? currentResult : combinedResult.Combine(currentResult);
-        //             }
-        //
-        //             // If the combined result is null, no consumptions even existed. We assume the message wasn't needed and can be acked.
-        //             code = combinedResult?.Code ?? ConsumptionResultCode.Ack;
-        //         }
-        //     }
-        //     catch (Exception ex)
-        //     {
-        //         code = configuration.RequeueOnException ? ConsumptionResultCode.NackRequeue : ConsumptionResultCode.Nack;
-        //         _logger.LogError(ex, "Receiving a message threw an exception.");
-        //     }
-        //
-        //     // Using a lock so the channel can be used safely in multiple threads
-        //     // Intentionally avoiding the cancellation token, because when stopping the ack or nack should still go through to avoid message noise in the queue.
-        //     await ackLock.WaitAsync(cancellationToken: CancellationToken.None);
-        //     
-        //     try
-        //     {
-        //         // Evaluate and act accordingly
-        //         if (code == ConsumptionResultCode.Ack)
-        //         {
-        //             await channel.BasicAckAsync(args.DeliveryTag, multiple: false, cancellationToken);
-        //         }
-        //         else
-        //         {
-        //             var requeue = code == ConsumptionResultCode.NackRequeue;
-        //             await channel.BasicNackAsync(args.DeliveryTag, multiple: false, requeue, cancellationToken);
-        //         }
-        //     }
-        //     finally
-        //     {
-        //         ackLock.Release();
-        //     }
-        // };
-
         // Configure consumption on that queue to the consumer (with the receive-event) and disable automatic acknowledging, since were doing that manually
         await channel.BasicConsumeAsync(
             queue: configuration.Queue,
