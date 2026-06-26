f8f926c98aab4956be7f1b686f4eb5bc3c82ee98
Nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-06-26 20:48:30 +0200

    prod management analysis repos

--- src/shopfloor/Mes.Shopfloor.Api.ProductionManagement/Subdomains/Analysis/Repositories/IProductionUnitStateRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using Mes.Shopfloor.Api.Infrastructure;
-
-namespace Mes.Shopfloor.Api.ProductionManagement.Subdomains.Analysis.Repositories;
-
-internal interface IProductionUnitStateRepository : IRepository
-{
-    Task<ProductionUnitState?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
-}
