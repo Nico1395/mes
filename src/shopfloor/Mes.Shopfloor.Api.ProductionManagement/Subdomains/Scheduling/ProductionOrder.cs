43ac5cdf9deaeb48b97cc92a9278409e0581b143
Nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-06-26 20:51:42 +0200

    scheduling domain

--- src/shopfloor/Mes.Shopfloor.Api.ProductionManagement/Subdomains/Scheduling/ProductionOrder.cs
+++ src/shopfloor/Mes.Shopfloor.Api.ProductionManagement/Subdomains/Scheduling/ProductionOrder.cs
@@ -1,16 +1,14 @@
-﻿using Mes.Shopfloor.Api.ProductionManagement.Subdomains.ProductDefinition;
-
-namespace Mes.Shopfloor.Api.ProductionManagement.Subdomains.DetailedScheduling;
+﻿namespace Mes.Shopfloor.Api.ProductionManagement.Subdomains.Scheduling;
 
 internal sealed class ProductionOrder
 {
     public Guid Id { get; init; } = Guid.NewGuid();
     public required Guid ProductId { get; init; }
-    public Product? Product { get; init; }
-    public required double Quantity { get; set; }
     public required string Name { get; set; }
     public string? Description { get; set; }
-    public required DateTime TargetDate { get; set; }
+    public ProductionOrderProgress? Progress { get; init; }
+    public ProductionOrderPriority Priority { get; init; } = ProductionOrderPriority.Lowest;
+    public ProductionOrderState State { get; set; } = ProductionOrderState.Defined;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
---
+++ src/shopfloor/Mes.Shopfloor.Api.ProductionManagement/Subdomains/Scheduling/ProductionOrderPriority.cs
@@ -0,0 +1,10 @@
+﻿namespace Mes.Shopfloor.Api.ProductionManagement.Subdomains.Scheduling;
+
+internal enum ProductionOrderPriority
+{
+    Highest = 0,
+    High = 1,
+    Medium = 2,
+    Low = 3,
+    Lowest = 4,
+}
