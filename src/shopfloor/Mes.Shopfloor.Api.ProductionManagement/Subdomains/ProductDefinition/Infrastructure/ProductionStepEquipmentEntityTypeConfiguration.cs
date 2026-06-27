1849b3d7cf265e156fd75d66f248e2aa07cb989d
Nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-06-27 09:00:28 +0200

    product def configs

---
+++ src/shopfloor/Mes.Shopfloor.Api.ProductionManagement/Subdomains/ProductDefinition/Infrastructure/ProductionStepEquipmentEntityTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Mes.Shopfloor.Api.ProductionManagement.Subdomains.ProductDefinition.Infrastructure;
+
+internal sealed class ProductionStepEquipmentEntityTypeConfiguration : IEntityTypeConfiguration<ProductionStepEquipment>
+{
+    public void Configure(EntityTypeBuilder<ProductionStepEquipment> builder)
+    {
+        builder.ToTable("production_step_equipment", "product_definition");
+        builder.HasKey(p => new { p.ProductionStepId, p.EquipmentId });
+        builder.Property(p => p.ProductionStepId).HasColumnName("production_step_id").IsRequired();
+        builder.Property(p => p.EquipmentId).HasColumnName("equipment_id").IsRequired();
+        builder.Property(p => p.Quantity).HasColumnName("quantity").IsRequired();
+        builder.HasOne(p => p.Equipment).WithMany().HasForeignKey(f => f.EquipmentId);
+    }
+}
