53444f9be5c128b4521d9de9ca57f4691a61ccfd
Nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-06-27 09:01:31 +0200

    get production order by id

---
+++ src/shopfloor/Mes.Shopfloor.Api.ProductionManagement/Subdomains/Scheduling/Repositories/ProductionOrderRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mes.Shopfloor.Api.ProductionManagement.Subdomains.Scheduling.Repositories;
+
+internal sealed class ProductionOrderRepository(DbContext _context) : IProductionOrderRepository
+{
+    public Task<ProductionOrder?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        return _context
+            .Set<ProductionOrder>()
+            .SingleOrDefaultAsync(o => o.Id == id, cancellationToken);
+    }
+
+    public Task<ProductionOrder?> GetByIdEagerAsync(Guid id, CancellationToken cancellationToken)
+    {
+        return _context
+            .Set<ProductionOrder>()
+            .Include(p => p.Progress)
+            .SingleOrDefaultAsync(o => o.Id == id, cancellationToken);
+    }
+}
