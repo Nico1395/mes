bba6f3212bb2e28b4125390bdd52ecd90caba0b0
Nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-06-27 09:01:16 +0200

    equipment contracts

---
+++ src/shopfloor/Mes.Shopfloor.Api.ProductionManagement/Subdomains/Resources/Contracts/EquipmentDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Mes.Shopfloor.Api.ProductionManagement.Subdomains.Resources.Contracts;
+
+internal sealed class EquipmentDto
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
