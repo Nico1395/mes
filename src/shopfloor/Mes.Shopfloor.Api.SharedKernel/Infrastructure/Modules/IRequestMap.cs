4393754ffd44483c97ff38d949031c268c9a4f9f
Nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-07-26 17:08:08 +0200

    removed legacy code
    Signed-off-by: Nico1395 <119791859+Nico1395@users.noreply.github.com>

--- src/shopfloor/Mes.Shopfloor.Api.SharedKernel/Infrastructure/Modules/IRequestMap.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Mes.Shopfloor.Api.SharedKernel.Infrastructure.Modules;
-
-public interface IRequestMap<TRequest, TResponse, TMapRequest, TMapResponse>
-{
-    TRequest Map(TMapRequest request);
-    TMapResponse Map(TResponse request);
-}
