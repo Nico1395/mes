3c3de14e0f30bcdcf332773ab14d5a6c17c6b539
Nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-07-28 18:44:14 +0200

    added shared contracts assemblies
    Signed-off-by: Nico1395 <119791859+Nico1395@users.noreply.github.com>

---
+++ src/shared/Mes.Shared.Contracts.SystemManagement/Parameterization/ParameterKeySto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Mes.Shared.Contracts.SystemManagement.Parameterization;
+
+public sealed class ParameterKeySto
+{
+    public required Guid Id { get; init; }
+    public required string Key { get; init; }
+    public List<ParameterValue>? Values { get; init; }
+}
---
+++ src/shared/Mes.Shared.Contracts.SystemManagement/Parameterization/ParameterValueSto.cs
@@ -0,0 +1,10 @@
+﻿namespace Mes.Shared.Contracts.SystemManagement.Parameterization;
+
+public sealed class ParameterValueSto
+{
+    public required string ParameterKey { get; init; }
+    public required string ShopfloorKey { get; init; }
+    public required string SerializedValue { get; init; }
+    public required DateTime CreatedAt { get; init; }
+    public required DateTime UpdatedAt { get; init; }
+}
